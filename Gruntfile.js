62eb462ad74a765247367d70490e3c086a7131af
Graham Gilchrist <graham@incuna.com>
2014-07-04 10:17:03 +0100

    Change to 4 space indenting

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@
 'use strict';
 
 module.exports = function(grunt) {
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
 
 };
